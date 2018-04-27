9b31bff6ba87cd2ec9c20e2cab53a32598e14437
Your Name <supuni.nimeshika@gmail.com>
2018-04-27 20:27:56 +0530

    Add Two Fragment Classes

---
+++ app/src/main/java/com/example/supuni/navigationdrawer/Helper/FragmentNavigationManager.java
@@ -0,0 +1,14 @@
+package com.example.supuni.navigationdrawer.Helper;
+
+import com.example.supuni.navigationdrawer.Interface.NavigationManager;
+
+/**
+ * Created by supuni on 4/27/18.
+ */
+
+public class FragmentNavigationManager implements NavigationManager {
+    @Override
+    public void showFragment(String title) {
+
+    }
+}
---
+++ app/src/main/java/com/example/supuni/navigationdrawer/Interface/NavigationManager.java
@@ -0,0 +1,9 @@
+package com.example.supuni.navigationdrawer.Interface;
+
+/**
+ * Created by supuni on 4/27/18.
+ */
+
+public interface NavigationManager {
+    void showFragment(String title);
+}
