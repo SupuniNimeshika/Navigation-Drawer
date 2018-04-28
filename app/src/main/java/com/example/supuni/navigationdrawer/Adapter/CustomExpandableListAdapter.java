954af7dbd616d6853dd5ea51ca5cd8481f0a8b3d
Your Name <supuni.nimeshika@gmail.com>
2018-04-28 19:21:46 +0530

    Add Navigation Manager

---
+++ app/src/main/java/com/example/supuni/navigationdrawer/Adapter/CustomExpandableListAdapter.java
@@ -0,0 +1,89 @@
+package com.example.supuni.navigationdrawer.Adapter;
+
+import android.content.Context;
+import android.graphics.Typeface;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseExpandableListAdapter;
+import android.widget.TextView;
+
+import com.example.supuni.navigationdrawer.R;
+
+import java.util.List;
+import java.util.Map;
+
+/**
+ * Created by supuni on 4/27/18.
+ */
+
+public class CustomExpandableListAdapter extends BaseExpandableListAdapter {
+
+    private Context context;
+    private List<String > listTitle;
+    private Map<String,List<String>> listItem;
+
+    public CustomExpandableListAdapter(Context context,List<String> listTitle,Map<String,List<String>> listItem){
+        this.context =context;
+        this.listTitle=listTitle;
+        this.listItem=listItem;
+    }
+
+    @Override
+    public int getGroupCount() {
+        return listTitle.size();
+    }
+
+    @Override
+    public int getChildrenCount(int groupPosition) {
+        return listItem.size();
+    }
+
+    @Override public Object getGroup(int groupPosition) {
+        return listTitle.get(groupPosition);
+    }
+
+    @Override
+    public Object getChild(int groupPosition, int childPosition) {
+        return listItem.get(listTitle.get(groupPosition)).get(childPosition);
+    }
+
+    @Override
+    public long getGroupId(int groupPosition) {
+        return groupPosition;
+    }
+
+    @Override
+    public long getChildId(int groupPosition, int childPosition) {
+        return childPosition;
+    }
+
+    @Override
+    public boolean hasStableIds() {
+        return false;
+    }
+
+    @Override
+    public View getGroupView(int groupPosition, boolean isExpanded, View convertView, ViewGroup parent) {
+        String title =(String)getGroup(groupPosition);
+        if(convertView ==null)
+        {
+            convertView = LayoutInflater.from(context).inflate(R.layout.layout_group,null);
+        }
+        TextView txtTitle =(TextView)convertView.findViewById(R.id.listTitle);
+        txtTitle.setTypeface(null, Typeface.BOLD);
+        txtTitle.setText(title);
+
+        return convertView;
+    }
+
+    @Override
+    public View getChildView(int i, int i1, boolean b, View view, ViewGroup viewGroup) {
+        return null;
+    }
+
+    @Override
+    public boolean isChildSelectable(int i, int i1) {
+        return false;
+    }
+}
--- app/src/main/java/com/example/supuni/navigationdrawer/Helper/FragmentNavigationManager.java
+++ app/src/main/java/com/example/supuni/navigationdrawer/Helper/FragmentNavigationManager.java
@@ -1,12 +1,33 @@
 package com.example.supuni.navigationdrawer.Helper;
 
+import android.support.v4.app.FragmentManager;
+
 import com.example.supuni.navigationdrawer.Interface.NavigationManager;
+import com.example.supuni.navigationdrawer.MainActivity;
 
 /**
  * Created by supuni on 4/27/18.
  */
 
 public class FragmentNavigationManager implements NavigationManager {
+
+    private static FragmentNavigationManager mInstance;
+
+    private FragmentManager mFragementManager;
+    private MainActivity mainActivity;
+
+    public static FragmentNavigationManager getmInstance (MainActivity mainActivity)
+    {
+        if(mInstance ==null)
+            mInstance =new FragmentNavigationManager();
+        mInstance.configure(mainActivity);
+        return mInstance;
+    }
+
+    private void configure(MainActivity mainActivity){
+        mainActivity =mainActivity;
+        mFragementManager =mainActivity.getSupportFragmentManager();
+    }
     @Override
     public void showFragment(String title) {
 
