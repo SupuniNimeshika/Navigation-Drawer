5daffb22a26d0399ee788963e9aaa524d673f5c2
Your Name <supuni.nimeshika@gmail.com>
2018-04-29 22:39:50 +0530

    Add Content For Navigation Drawer Menu

--- app/src/main/java/com/example/supuni/navigationdrawer/MainActivity.java
+++ app/src/main/java/com/example/supuni/navigationdrawer/MainActivity.java
@@ -1,19 +1,28 @@
 package com.example.supuni.navigationdrawer;
 
 import android.app.ExpandableListActivity;
+import android.app.FragmentManager;
+import android.support.v4.view.GravityCompat;
 import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
 import android.widget.ExpandableListAdapter;
 import android.widget.ExpandableListView;
 
+import com.example.supuni.navigationdrawer.Adapter.CustomExpandableListAdapter;
+import com.example.supuni.navigationdrawer.Helper.FragmentNavigationManager;
 import com.example.supuni.navigationdrawer.Interface.NavigationManager;
 
+import java.lang.reflect.Array;
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 import java.util.Map;
+import java.util.TreeMap;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -32,6 +41,117 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        //init view
+        mDrawerLayout =(DrawerLayout)findViewById(R.id.drawer_layout);
+        mActivityTitle =getTitle().toString();
+        expandableListView =(ExpandableListView)findViewById(R.id.navList);
+        navigationManager = FragmentNavigationManager.getmInstance(this);
+
+        initItems();
+
+        View listHeaderView =getLayoutInflater().inflate(R.layout.nav_header,null,false);
+        expandableListView.addHeaderView(listHeaderView);
+
+        genData();
+
+        addDrawersItem();
+        setupDrawer();
+        
+        if(savedInstanceState == null)
+            selectFirstItemDefault();
+
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        getSupportActionBar().setHomeButtonEnabled(true);
+        getSupportActionBar().setTitle("EDMTDev");
+    }
+
+    private void selectFirstItemDefault() {
+        if(navigationManager !=null)
+        {
+            String firstItem =lstTitle.get(0);
+            navigationManager.showFragment(firstItem);
+            getSupportActionBar().setTitle(firstItem);
+        }
+    }
+
+    private void setupDrawer() {
+        mDrawerToggle =new ActionBarDrawerToggle(this,mDrawerLayout,R.string.open,R.string.close)
+        {
+            @Override
+            public void onDrawerOpened(View drawerView) {
+                super.onDrawerOpened(drawerView);
+                getSupportActionBar().setTitle("EDMTDev");
+                invalidateOptionsMenu();
+            }
+
+            @Override
+            public void onDrawerClosed(View drawerView) {
+                super.onDrawerClosed(drawerView);
+                getSupportActionBar().setTitle(mActivityTitle);
+                invalidateOptionsMenu();
+            }
+        };
+
+        mDrawerToggle.setDrawerIndicatorEnabled(true);
+        mDrawerLayout.setDrawerListener(mDrawerToggle);
+    }
+
+    private void addDrawersItem() {
+        adapter = new CustomExpandableListAdapter(this, lstTitle, lstChild);
+        expandableListView.setAdapter(adapter);
+        expandableListView.setOnGroupExpandListener(new ExpandableListView.OnGroupExpandListener() {
+
+            @Override
+            public void onGroupExpand(int groupPosition) {
+                getSupportActionBar().setTitle(lstTitle.get(groupPosition).toString());//Set title for Toolbar when open menu
+            }
+        });
+
+        expandableListView.setOnGroupCollapseListener(new ExpandableListView.OnGroupCollapseListener() {
+            @Override
+            public void onGroupCollapse(int groupPosition) {
+                getSupportActionBar().setTitle("EDMTDEv");
+            }
+        });
+
+        expandableListView.setOnChildClickListener(new ExpandableListView.OnChildClickListener() {
+            @Override
+            public boolean onChildClick(ExpandableListView parent, View v, int groupPosition, int childPosition, long l) {
+
+                //Change fragment when click on item
+                String selectedItem =((List)(lstChild.get(lstTitle.get(groupPosition))))
+                        .get(childPosition).toString();
+                getSupportActionBar().setTitle(selectedItem);
+
+                if(items[0].equals(lstTitle.get(groupPosition)))
+                    navigationManager.showFragment(selectedItem);
+                else
+                    throw new IllegalArgumentException("Not supported fragment");
+
+                mDrawerLayout.closeDrawer(GravityCompat.START);
+                return false;
+            }
+        });
+    }
+
+    private void genData(){
+
+        List<String> title = Arrays.asList("Android Programming","Xamarin Programming","iOS Programming");
+        List<String> childitem =Arrays.asList("Beginner","Intermediate","Advanced","Professional");
+
+        lstChild =new TreeMap<>();
+        lstChild.put(title.get(0),childitem);
+        lstChild.put(title.get(1),childitem);
+        lstChild.put(title.get(2),childitem);
+
+        lstTitle =new ArrayList<>(lstChild.keySet());
+
+    }
+
+    private void initItems(){
+
+        items =new String[]{"Android Programming","Xamarin Programming","iOS Programming"};
     }
 
 
