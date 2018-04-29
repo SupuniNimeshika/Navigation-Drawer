8c7375cb4e3755e77539b4e1eb157415cac48a8e
Your Name <supuni.nimeshika@gmail.com>
2018-04-29 19:34:36 +0530

    Add Variable In Main Activity

--- app/src/main/java/com/example/supuni/navigationdrawer/MainActivity.java
+++ app/src/main/java/com/example/supuni/navigationdrawer/MainActivity.java
@@ -1,13 +1,49 @@
 package com.example.supuni.navigationdrawer;
 
+import android.app.ExpandableListActivity;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.ExpandableListAdapter;
+import android.widget.ExpandableListView;
+
+import com.example.supuni.navigationdrawer.Interface.NavigationManager;
+
+import java.util.List;
+import java.util.Map;
 
 public class MainActivity extends AppCompatActivity {
+
+    private DrawerLayout mDrawerLayout;
+    private ActionBarDrawerToggle mDrawerToggle;
+    private String mActivityTitle;
+    private String [] items;
+
+    private ExpandableListView expandableListView;
+    private ExpandableListAdapter adapter;
+    private List<String> lstTitle;
+    private Map<String,List<String>> lstChild;
+    private NavigationManager navigationManager;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.main_menu,menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        int id =item.getItemId();
+        return super.onOptionsItemSelected(item);
     }
 }
